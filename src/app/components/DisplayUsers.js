5455dbea6ecea2fb0e31805db6b3de0f9bf38caf
Shahrukh <shahrukh1494@gmail.com>
2024-03-15 18:00:19 +0530

    get data from redux store

--- src/app/components/DisplayUsers.js
+++ src/app/components/DisplayUsers.js
@@ -1,9 +1,19 @@
+"use client"
+import { useSelector } from "react-redux"
+
 const DisplayUsers = () => {
-  return (
-    <div className="display-user">
-        <h1>User List</h1>
-    </div>
-  )
+    const userData = useSelector((data) => data.users)
+
+    return (
+        <div className="display-user">
+            <h1>User List</h1>
+            {
+                userData.map((user) => 
+                    <div key={user.id} className="user-single">{user.name}</div>
+                )
+            }
+        </div>
+    )
 }
 
 export default DisplayUsers
